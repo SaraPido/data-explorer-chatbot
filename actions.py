8b72e8a1749fc6e69faab1c77b8cbb3df1400e4d
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2018-11-12 16:21:59 +0100

    new actions to handle the validation and reset cases

--- actions.py
+++ actions.py
@@ -10,6 +10,69 @@ from __future__ import unicode_literals
 from rasa_core_sdk import Action
 from rasa_core_sdk.events import SlotSet
 
+class ActionGetLessonsOfTeacherName(Action):
+
+	def name(self):
+		return 'action_get_lessons_of_teacher_name'
+
+	def run(self, dispatcher, tracker, domain):
+		return [
+			SlotSet("teacher_id", 27),
+			SlotSet("lesson_list", [
+				{'id':0,'name':'Maths'},
+				{'id':1,'name':'Italian'},
+				{'id':2,'name':'I do not know'},
+			])
+		]
+
+
+class ActionValidateTeacherName(Action):
+
+	def name(self):
+		return 'action_validate_teacher_name'
+
+	def run(self, dispatcher, tracker, domain):
+		teacher_name = tracker.get_slot('teacher_name')
+		if teacher_name.lower() == 'nicola': #just to make an example
+			return [SlotSet("found_name", teacher_name)]
+		print('I received invalid name: '+teacher_name)
+		return []
+
+class ActionValidateLessonName(Action):
+
+	def name(self):
+		return 'action_validate_lesson_name'
+
+	def run(self, dispatcher, tracker, domain):
+		lesson_name = tracker.get_slot('lesson_name')
+		lesson_list = tracker.get_slot('lesson_list')
+		if lesson_list!=None and any(l['name'] == lesson_name for l in lesson_list):
+			return [SlotSet("found_name", lesson_name)]
+		return []
+
+class ActionResetFoundName(Action):
+	def name(self):
+		return 'action_reset_found_name'
+	def run(self, dispatcher, tracker, domain):
+		return [SlotSet("found_name", None)]
+
+
+class ActionGetButtonsOfLessonList(Action):
+
+	def name(self):
+		return 'action_get_buttons_of_lesson_list'
+
+	def run(self, dispatcher, tracker, domain):
+		lesson_list = tracker.get_slot('lesson_list')
+		create_button = lambda e: {'title':e['name'], 'payload':'/choose{"lesson_name": "'+e['name']+'"}'}
+		buttons = list(map(create_button, lesson_list))
+		dispatcher.utter_button_message('',buttons)
+		return []
+
+
+
+
+'''
 import sqlite3
 from sqlite3 import Error
 
@@ -87,3 +150,4 @@ class ActionFilterByAttribute(Action):
 		attribute_name = tracker.get_slot('attribute_name')
 		dispatcher.utter_message('Good job, you chose '+ attribute_name)
 		return []
+'''
